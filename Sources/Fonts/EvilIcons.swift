247347156ed870603079679ebf3f57d0ef7d941a
Quan <dinhquan191@gmail.com>
2021-12-02 07:34:52 +0700

    Add font

---
+++ Sources/Fonts/EvilIcons.swift
@@ -0,0 +1,83 @@
+//
+//  EvilIcons.swift
+//  VectorIcons
+//
+//  Created by Quan on 15/11/2021.
+//
+
+import Foundation
+
+extension IconFont {
+    public enum EvilIcons: UInt32 {
+        case archive = 61696
+        case arrow_down = 61697
+        case arrow_left = 61698
+        case arrow_right = 61699
+        case arrow_up = 61700
+        case bell = 61701
+        case calendar = 61702
+        case camera = 61703
+        case cart = 61704
+        case chart = 61705
+        case check = 61706
+        case chevron_down = 61707
+        case chevron_left = 61708
+        case chevron_right = 61709
+        case chevron_up = 61710
+        case clock = 61711
+        case close = 61712
+        case close_o = 61713
+        case comment = 61714
+        case credit_card = 61715
+        case envelope = 61716
+        case exclamation = 61717
+        case external_link = 61718
+        case eye = 61719
+        case gear = 61720
+        case heart = 61721
+        case image = 61722
+        case like = 61723
+        case link = 61724
+        case location = 61725
+        case lock = 61726
+        case minus = 61727
+        case navicon = 61728
+        case paperclip = 61729
+        case pencil = 61730
+        case play = 61731
+        case plus = 61732
+        case pointer = 61733
+        case question = 61734
+        case redo = 61735
+        case refresh = 61736
+        case retweet = 61737
+        case sc_facebook = 61738
+        case sc_github = 61739
+        case sc_google_plus = 61740
+        case sc_instagram = 61741
+        case sc_linkedin = 61742
+        case sc_odnoklassniki = 61743
+        case sc_pinterest = 61744
+        case sc_skype = 61745
+        case sc_soundcloud = 61746
+        case sc_telegram = 61747
+        case sc_tumblr = 61748
+        case sc_twitter = 61749
+        case sc_vimeo = 61750
+        case sc_vk = 61751
+        case sc_youtube = 61752
+        case search = 61753
+        case share_apple = 61754
+        case share_google = 61755
+        case spinner = 61756
+        case spinner_2 = 61757
+        case spinner_3 = 61758
+        case star = 61759
+        case tag = 61760
+        case trash = 61761
+        case trophy = 61762
+        case undo = 61763
+        case unlock = 61764
+        case user = 61765
+    }
+}
---
+++ Sources/Fonts/Zocial.swift
@@ -0,0 +1,113 @@
+//
+//  Zocial.swift
+//  VectorIcons
+//
+//  Created by Quan on 15/11/2021.
+//
+
+import Foundation
+
+extension IconFont {
+    public enum Zocial: UInt32 {
+        case acrobat = 61696
+        case amazon = 61697
+        case android = 61698
+        case angellist = 61699
+        case aol = 61700
+        case appnet = 61701
+        case appstore = 61702
+        case bitbucket = 61703
+        case bitcoin = 61704
+        case blogger = 61705
+        case buffer = 61706
+        case cal = 61707
+        case call = 61708
+        case cart = 61709
+        case chrome = 61710
+        case cloudapp = 61711
+        case creativecommons = 61712
+        case delicious = 61713
+        case digg = 61714
+        case disqus = 61715
+        case dribbble = 61716
+        case dropbox = 61717
+        case drupal = 61718
+        case dwolla = 61720
+        case email = 61721
+        case eventasaurus = 61722
+        case eventbrite = 61723
+        case eventful = 61724
+        case evernote = 61725
+        case facebook = 61726
+        case fivehundredpx = 61727
+        case flattr = 61728
+        case flickr = 61729
+        case forrst = 61730
+        case foursquare = 61731
+        case github = 61732
+        case gmail = 61733
+        case google = 61734
+        case googleplay = 61735
+        case googleplus = 61736
+        case gowalla = 61737
+        case grooveshark = 61738
+        case guest = 61739
+        case html5 = 61740
+        case ie = 61741
+        case instagram = 61742
+        case instapaper = 61743
+        case intensedebate = 61744
+        case itunes = 61745
+        case klout = 61746
+        case lanyrd = 61747
+        case lastfm = 61748
+        case lego = 61749
+        case linkedin = 61750
+        case lkdto = 61751
+        case logmein = 61752
+        case macstore = 61753
+        case meetup = 61754
+        case myspace = 61755
+        case ninetyninedesigns = 61756
+        case openid = 61757
+        case opentable = 61758
+        case paypal = 61759
+        case persona = 61796
+        case pinboard = 61760
+        case pinterest = 61761
+        case plancast = 61762
+        case plurk = 61763
+        case pocket = 61764
+        case podcast = 61765
+        case posterous = 61766
+        case print = 61767
+        case quora = 61768
+        case reddit = 61769
+        case rss = 61770
+        case scribd = 61771
+        case skype = 61772
+        case smashing = 61773
+        case songkick = 61774
+        case soundcloud = 61775
+        case spotify = 61776
+        case stackoverflow = 61777
+        case statusnet = 61778
+        case steam = 61779
+        case stripe = 61780
+        case stumbleupon = 61781
+        case tumblr = 61782
+        case twitter = 61783
+        case viadeo = 61784
+        case vimeo = 61785
+        case vk = 61786
+        case weibo = 61787
+        case wikipedia = 61788
+        case windows = 61789
+        case wordpress = 61790
+        case xing = 61791
+        case yahoo = 61792
+        case ycombinator = 61793
+        case yelp = 61794
+        case youtube = 61795
+    }
+}
