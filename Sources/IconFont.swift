5eb63700b6e4b1c9396d4f049d02a5bce62f53df
Quan <dinhquan191@gmail.com>
2021-12-02 07:42:26 +0700

    Implement all fonts

--- Sources/IconFont.swift
+++ Sources/IconFont.swift
@@ -10,6 +10,18 @@ import Foundation
 public enum IconFont {
     case antDesign(AntDesign)
     case entypo(Entypo)
+    case evilIcons(EvilIcons)
+    case feather(Feather)
+    case fontAwesome(FontAwesome)
+    case fontAewsome5(FontAwesome5)
+    case fontisto(Fontisto)
+    case foundation(Foundation)
+    case ionicons(Ionicons)
+    case materialCommunityIcons(MaterialCommunityIcons)
+    case materialIcons(MaterialIcons)
+    case octicons(Octicons)
+    case simpleLineIcons(SimpleLineIcons)
+    case zocial(Zocial)
 }
 
 extension IconFont {
@@ -19,22 +31,71 @@ extension IconFont {
             return "anticon"
         case .entypo:
             return "Entypo"
+        case .evilIcons:
+            return "EvilIcons"
+        case .feather:
+            return "Feather"
+        case .fontAwesome:
+            return "FontAwesome"
+        case .fontAewsome5:
+            return "Font Awesome 5 Free"
+        case .fontisto:
+            return "fontisto"
+        case .foundation:
+            return "Foundation"
+        case .ionicons:
+            return "Ionicons"
+        case .materialCommunityIcons:
+            return "Material Design Icons"
+        case .materialIcons:
+            return "Material Icons"
+        case .octicons:
+            return "Octicons"
+        case .simpleLineIcons:
+            return "simple-line-icons"
+        case .zocial:
+            return "Zocial"
         }
     }
     
     var text: String {
         switch self {
-        case .antDesign(let antDesign):
-            return antDesign.rawValue.unicodeString
-        case .entypo(let entypo):
-            return entypo.rawValue.unicodeString
+        case .antDesign(let value):
+            return value.rawValue.unicodeString
+        case .entypo(let value):
+            return value.rawValue.unicodeString
+        case .evilIcons(let value):
+            return value.rawValue.unicodeString
+        case .feather(let value):
+            return value.rawValue.unicodeString
+        case .fontAwesome(let value):
+            return value.rawValue.unicodeString
+        case .fontAewsome5(let value):
+            return value.rawValue.unicodeString
+        case .fontisto(let value):
+            return value.rawValue.unicodeString
+        case .foundation(let value):
+            return value.rawValue.unicodeString
+        case .ionicons(let value):
+            return value.rawValue.unicodeString
+        case .materialCommunityIcons(let value):
+            return value.rawValue.unicodeString
+        case .materialIcons(let value):
+            return value.rawValue.unicodeString
+        case .octicons(let value):
+            return value.rawValue.unicodeString
+        case .simpleLineIcons(let value):
+            return value.rawValue.unicodeString
+        case .zocial(let value):
+            return value.rawValue.unicodeString
         }
     }
 }
 
 private extension UInt32 {
     var unicodeString: String {
-        String(UnicodeScalar(self)!)
+        let value = self % 1000000
+        return String(UnicodeScalar(value)!)
     }
 }
 
